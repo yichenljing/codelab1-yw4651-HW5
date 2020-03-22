34212ff5343af481a41c93c31578460d0aa8f1b4
yichenljing <40904846+yichenljing@users.noreply.github.com>
2020-03-22 18:12:41 -0400

    add colliders for game objects

---
+++ codelab-yw4651-HW5/Assets/Scripts/AsciiLevelLoader.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsciiLevelLoader : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject prize;
+    public GameObject player;
+
+    public float xOffset = -5;
+    public float yOffset = 5;
+
+    public string fileLevel = "Level.txt"; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string fullFilePath = Application.dataPath + "/" + fileLevel;
+
+        print("Full file path: " + fullFilePath); // print the path
+
+        print(File.ReadAllText(fullFilePath));   // print the contents of the file
+
+        string[] lines = File.ReadAllLines(fullFilePath); // line = an array of syrings
+
+        GameObject wallHolder = new GameObject("wall Holder");
+
+        for (int y = 0; y < lines.Length; y++) {
+            string line = lines[y];  //get throgh eacch line
+
+            char[] characters = line.ToCharArray(); // get throgh each character
+
+        for (int x = 0; x < characters.Length; x++){
+                GameObject newObject;
+
+                switch (characters[x])
+                { 
+                    case 'W':
+
+                        newObject = Instantiate<GameObject>(wall);
+                        newObject.transform.SetParent(wallHolder.transform);
+
+                        newObject.transform.position =
+                                new Vector2(x + xOffset, -y + yOffset);
+                        break;
+
+                    case 'P':
+                        newObject = Instantiate<GameObject>(player);
+                        newObject.transform.position =
+                                new Vector2(x + xOffset, -y + yOffset);
+                        break;
+                    case '*':
+                        newObject = Instantiate<GameObject>(prize);
+                        newObject.transform.position =
+                                     new Vector2(x + xOffset, -y + yOffset);
+                        break;
+
+                    default:
+                        print("empty");
+                        break;
+
+
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ codelab-yw4651-HW5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force = 5;
+    Rigidbody2D rb;
+
+
+    public static PlayerController instance; //this static var will hold the Singleton
+
+    private void Awake()
+    {
+        if (instance == null) //instance hasn't been set yet
+        {
+            instance = this; // set instance to this object
+
+            DontDestroyOnLoad(gameObject); //Dont Destroy this object when you load a new scene
+
+        }
+        else
+        { Destroy(gameObject); } // destroy this new object, so there is only one
+    }
+
+
+    private void Start()
+    {
+        Debug.Log("Hello, world!");
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()  //draw
+    {
+        Debug.Log("Goodbye, world!");
+
+    
+        if (Input.GetKey(KeyCode.D)) {   // if D is pressed
+
+            rb.AddForce(Vector2.right * force);   // apply a force using the "force" var
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            rb.AddForce(Vector2.left * force);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+
+            rb.AddForce(Vector2.down * force);
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+
+            rb.AddForce(Vector2.up * force);
+
+        }
+    }
+}
