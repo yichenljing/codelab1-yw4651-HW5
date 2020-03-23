5c475375c5527aa9fabe254f58048bf095bec810
yichenljing <40904846+yichenljing@users.noreply.github.com>
2020-03-23 09:23:41 -0400

    Fix display issues of prize

---
+++ codelab-yw4651-HW5/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class ChangeColor : MonoBehaviour
+{
+    public Color colorA;
+    public Color colorB;
+
+
+    private SpriteRenderer spriteRenderer;
+
+    public float timeTochange = 0.1f; //how often colors change
+    private float StartTime = 0f;  //when to start count time
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        StartTime += Time.deltaTime;
+
+        if (spriteRenderer.color != colorA && StartTime >= timeTochange) //change color when it's time
+        {
+
+            spriteRenderer.color = colorA;
+            StartTime = 0f;  //reset the start time to 0
+        }
+        else if (spriteRenderer.color != colorB && StartTime >= timeTochange)
+        {
+
+            spriteRenderer.color = colorB;
+            StartTime = 0f;
+        }
+    }
+}
